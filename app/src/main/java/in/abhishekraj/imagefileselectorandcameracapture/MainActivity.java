98f51113d46ead0ea4987cc61c0e497923f5eb4f
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2017-04-15 12:54:20 +0530

    Connected both Activities using onItemClickListener

--- app/src/main/java/in/abhishekraj/imagefileselectorandcameracapture/MainActivity.java
+++ app/src/main/java/in/abhishekraj/imagefileselectorandcameracapture/MainActivity.java
@@ -1,7 +1,10 @@
 package in.abhishekraj.imagefileselectorandcameracapture;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddNewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
